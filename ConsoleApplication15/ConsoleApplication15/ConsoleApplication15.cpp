9d61543c2d17c655e9e922d06d62d7e90ffe4114
Fayt Dollard_ <dollardfayt@gmail.com>
2025-11-22 14:26:50 +0500

    lad-9

---
+++ ConsoleApplication15/ConsoleApplication15/ConsoleApplication15.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+
+class String {
+private:
+    std::string data;
+
+public:
+    String() = default;
+    String(const std::string& str) : data(str) {}
+    String(const char* str) : data(str) {}
+
+    char& operator[](size_t index) {
+        return data[index];
+    }
+
+    int operator()(char c) {
+        size_t pos = data.find(c);
+        if (pos != std::string::npos) {
+            return static_cast<int>(pos);
+        }
+        else {
+            return -1;
+        }
+    }
+
+    explicit operator int() const {
+        return static_cast<int>(data.length());
+    }
+};
+
+int main() {
+    String str("Hello");
+    std::cout << str[0] << std::endl;
+    std::cout << str('l') << std::endl;
+    std::cout << static_cast<int>(str) << std::endl;
+    return 0;
+}
